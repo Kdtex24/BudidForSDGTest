57a4905aee1b78f51c02ecb2558751e50b847e1e
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-08 16:45:48 +0100

    Challange 1 first try

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,48 @@
-const covid19ImpactEstimator = (data) => data;
+const data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+};
 
-export default covid19ImpactEstimator;
+
+const convertToDays = (periodType, timeToElapse) => {
+    switch (periodType) {
+        case 'days':
+            return timeToElapse;
+        case 'weeks':
+            return timeToElapse * 7;
+        case 'months':
+            return timeToElapse * 30
+    }
+};
+
+const covid19ImpactEstimator = (data) => {
+    const outputData = {
+        data: {},
+        impact: {},
+        severeImpact: {}
+    };
+
+    const {timeToElapse, reportedCases, periodType} = data;
+    const {impact, severeImpact} = outputData;
+    impact.currentlyInfected = reportedCases * 10;
+    severeImpact.currentlyInfected = reportedCases * 50;
+    let days = convertToDays(periodType, timeToElapse);
+    let factor = parseInt(days / 3);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (Math.pow(2, factor));
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (Math.pow(2, factor));
+    return outputData
+
+};
+
+console.log(covid19ImpactEstimator(data));
+
+// export default covid19ImpactEstimator;
