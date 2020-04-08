e31845ecdc79405b6a856269d5c56da493ebeded
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-08 16:46:17 +0100

    Challange 1 first try

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,16 @@
-const data = {
-    region: {
-        name: "Africa",
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 5,
-        avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-};
+// const data = {
+//     region: {
+//         name: "Africa",
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// };
 
 
 const convertToDays = (periodType, timeToElapse) => {
@@ -43,6 +43,6 @@ const covid19ImpactEstimator = (data) => {
 
 };
 
-console.log(covid19ImpactEstimator(data));
+// console.log(covid19ImpactEstimator(data));
 
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
