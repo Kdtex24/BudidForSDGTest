4c3643cd993e81e583def31eaa6de83b00fb0b38
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-12 20:23:42 +0100

    Cleaning up

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,3 @@
-// const data = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 4,
-//     avgDailyIncomePopulation: 0.73
-//   },
-//   periodType: 'days',
-//   timeToElapse: 38,
-//   reportedCases: 2747,
-//   population: 92931687,
-//   totalHospitalBeds: 678874
-// };
-
 const convertToDays = (periodType, timeToElapse) => {
   switch (periodType) {
     case 'weeks':
