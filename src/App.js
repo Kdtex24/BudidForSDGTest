987190c53f34d2c9cf753ebfe32bdfb0ddda3b50
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-13 16:00:51 +0100

    Removed modal

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React, { useState } from "react"
-import Form from "react-bootstrap/Form"
-import covid19ImpactEstimator from './estimator'
-import ModalComponent from './components/Modal'
+import React from "react"
+import Form from "react-bootstrap/Form";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
-      const data = {
-        region: {
-          name: "Africa",
-          avgAge: 19.7,
-          avgDailyIncomeInUSD: 4,
-          avgDailyIncomePopulation: 0.73,
-        },
-        periodType: periodType,
-        timeToElapse: parseInt(timeToElapse),
-        reportedCases: parseInt(reportedCases),
-        population: parseInt(population),
-        totalHospitalBeds: parseInt(totalHospitalBeds),
-    }
-    setModalData(covid19ImpactEstimator(data))
+    //   const data = {
+    //     region: {
+    //       name: "Africa",
+    //       avgAge: 19.7,
+    //       avgDailyIncomeInUSD: 4,
+    //       avgDailyIncomePopulation: 0.73,
+    //     },
+    //     periodType: periodType,
+    //     timeToElapse: parseInt(timeToElapse),
+    //     reportedCases: parseInt(reportedCases),
+    //     population: parseInt(population),
+    //     totalHospitalBeds: parseInt(totalHospitalBeds),
+    // }
     
     
   }
-  const [population, setPopulation] = useState(0)
-  const [timeToElapse, setTimeToElapse] = useState(0)
-  const [reportedCases, setReportedCases] = useState(0)
-  const [totalHospitalBeds, setTotalHospitalBeds] = useState(0)
-  const [periodType, setPeriodType] = useState('days')
-  const [modalData, setModalData] = useState(null)
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  
+  // const [population, setPopulation] = useState(0)
+  // const [timeToElapse, setTimeToElapse] = useState(0)
+  // const [reportedCases, setReportedCases] = useState(0)
+  // const [totalHospitalBeds, setTotalHospitalBeds] = useState(0)
+  // const [periodType, setPeriodType] = useState('days')
+
 
   return (
     <>
     <div className='container'>
       <div className="d-flex flex-column justify-content-center">
         <header className='text-center'>
-          <h1>Novelty COVID-19 Infections Estimator</h1>
+          <h1 className='mt-5'>Novelty COVID-19 Infections Estimator</h1>
         </header>
         <Form onSubmit={handleSubmit}>
           <div className='form-row'>
@@ -54,7 +50,7 @@ function App() {
               placeholder="Enter Population"
               data-population
               required
-              onChange={(e) => setPopulation(e.target.value)}
+              // onChange={(e) => setPopulation(e.target.value)}
             />
           </Form.Group>
 
@@ -68,7 +64,7 @@ function App() {
               placeholder="Time to Elapse"
               data-time-to-elapse
               required
-              onChange={(e) => setTimeToElapse(e.target.value)}
+              // onChange={(e) => setTimeToElapse(e.target.value)}
 
             />
           </div>
@@ -85,7 +81,7 @@ function App() {
               placeholder="Reported Cases"
               data-reported-cases
               required
-              onChange={(e) => setReportedCases(e.target.value)}
+              // onChange={(e) => setReportedCases(e.target.value)}
 
             />
           </div>
@@ -99,24 +95,23 @@ function App() {
               placeholder="Total Hospital Beds"
               data-total-hospital-beds
               required
-              onChange={(e) => setTotalHospitalBeds(e.target.value)}
+              // onChange={(e) => setTotalHospitalBeds(e.target.value)}
 
             />
           </div>
           <Form.Group controlId="periodType" className='col-md-3'>
             <Form.Label>Period Type</Form.Label>
-            <Form.Control data-period-type as="select" required onChange={(e) => setPeriodType(e.target.value)} >
+            <Form.Control data-period-type as="select" required >
               <option data-period-type value='days'>Days</option>
               <option data-period-type value='weeks'>Weeks</option>
               <option data-period-type value='months'>Months</option>
             </Form.Control>
           </Form.Group>
           </div>
-          <button type="submit" data-go-estimate className="btn btn-primary" onClick={handleShow} style={{backgroundColor:'#046B99', borderColor:'#046B99'}}>
+          <button type="submit" data-go-estimate className="btn btn-primary" style={{backgroundColor:'#046B99', borderColor:'#046B99'}}>
             Submit
           </button>
         </Form>
-        <ModalComponent  show={show} handleClose={handleClose} data={modalData}/>
       </div>
       </div>
     </>
